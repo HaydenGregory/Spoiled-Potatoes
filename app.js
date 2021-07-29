43568a2dbe33ae5b3b47776b1020a01d95ad3127
Janine Luongo <janineluongo@gmail.com>
2021-07-29 16:55:20 -0400

    added routes

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+const session = require('express-session');
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: "secret", // used to sign the cookie
+    resave: false, // update session even w/ no changes
+    saveUninitialized: true, // always create a session
+    cookie: {
+      secure: false, // true: only accept https req's
+      maxAge: 2592000, // time in seconds aka 30 days
+    },
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,76 @@
-var express = require('express');
+var express = require("express");
+// using bcrypt here to hash password
+const bcrypt = require("bcrypt");
+// ../ because we're in the routes folder and we need to go up a folder ot go to the models folder
+const db = require("../models");
+const { route } = require(".");
 var router = express.Router();
-const db = require('../models')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Test');
+// registering a user
+router.post("/register", function (req, res, next) {
+  // check if email and password have data entered
+  // if not(no) body or email or password respond with an error
+  if (!req.body || !req.body.email || !req.body.password) {
+    res.status(422).json({ error: "must enter email and password" });
+    return;
+  }
+  // hash password
+  bcrypt
+    .hash(req.body.password, 10)
+    // .then is a function that takes the hash we just created
+    .then((hash) => {
+      // store user details
+      db.User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash,
+      })
+        // then takes that user just created and passes it to the function
+        .then((user) => {
+          // respond with success
+          res.status(201).json(user);
+        });
+    });
 });
+
+// still sending info so doing another post
+// POST users/login
+router.post("/login", (req, res) => {
+  // what info do we need to log the user in?
+  if (!req.body || !req.body.email || !req.body.password) {
+    res.status(422).json({ error: "must enter email and password" });
+    return;
+  }
+
+  // find user, pass object into ()
+  db.User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((user) => {
+    // check password (check what pw the user input against stored password)
+    bcrypt.compare(req.body.password, user.password).then((success) => {
+      if (success) {
+        // log in user
+        // setting new property (of user) on this session object 
+        // read as: if there is a user on the session (read from right to left)
+        req.session.user = user;
+        res.json({ message: "successfully logged in" });
+      } else {
+        // if incorrect, 401 (unauthorized)
+        res.status(401).json({ error: "incorrect password" });
+      }
+    });
+  });
+});
+
+// get bc we arent sending info, just making request
+router.get('/logout', (req, res) => {
+  // tell express that user logged out
+  // remove user information from the session by setting user = null
+  req.session.user = null; 
+  // send response to show successful logout
+  res.json({ message: 'successfully logged out'})
+})
 
 module.exports = router;
